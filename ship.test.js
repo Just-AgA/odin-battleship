b4d445020aaccfed249f614378e8fbee7d6602ba
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-07-07 20:17:36 +0200

    Add new test cases for the hit and isSunk methods

--- ship.test.js
+++ ship.test.js
@@ -2,5 +2,9 @@ import Ship from './ship';
 
 test('new instance of ship has proper parameters', () => {
   const ship = Ship(3);
-  expect(ship.length).toBe(3);
+  expect(ship.hit()).toBe(1);
+  expect(ship.hit()).toBe(2);
+  expect(ship.isSunk()).toBe(false);
+  expect(ship.hit()).toBe(3);
+  expect(ship.isSunk()).toBe(true);
 });
