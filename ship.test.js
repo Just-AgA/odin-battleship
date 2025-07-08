c01601abee56ad5c10970c092b8b835546e7ba77
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-07-08 20:20:35 +0200

    Add test case to check ship coordinates

--- ship.test.js
+++ ship.test.js
@@ -5,4 +5,15 @@ describe('Ship Factory Function', () => {
     expect(() => Ship(0)).toThrow('Ship length must be positive.');
     expect(() => Ship(-2)).toThrow('Ship length must be positive.');
   });
+
+  test('sets and gets coordinates correctly', () => {
+    const ship = Ship(3);
+    const coords = [
+      [0, 0],
+      [0, 1],
+      [0, 2],
+    ];
+    ship.setCoordinates(coords);
+    expect(ship.getCoordinates()).toEqual(coords);
+  });
 });
