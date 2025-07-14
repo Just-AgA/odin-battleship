b2ab7c4ef25cc7738da66b28d459e0f697dd1eca
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-07-14 20:44:02 +0200

    Test if ships can be placed correctly

---
+++ src/tests/gameboard.test.js
@@ -0,0 +1,17 @@
+const Ship = require('../ship');
+const Gameboard = require('../gameboard');
+
+describe('Gameboard', () => {
+  let gameboard;
+
+  beforeEach(() => {
+    gameboard = Gameboard();
+  });
+
+  test('places ship correctly', () => {
+    const ship = Ship(3);
+    const placed = gameboard.placeShip(ship, 0, 0, true);
+    expect(placed).toBe(true);
+    expect(gameboard.getShips()).toContain(ship);
+  });
+});
