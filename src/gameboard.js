650e1c5083d64e37136d5f95b6d585299649cd3e
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-07-15 20:25:49 +0200

    Add nethod that checks the placement coordinates are occupied

--- src/gameboard.js
+++ src/gameboard.js
@@ -7,6 +7,12 @@ const Gameboard = () => {
   const isValidCoord = (x, y) => {
     return x >= 0 && x < boardSize && y >= 0 && y < boardSize;
   };
+
+  const isOccupied = (x, y) => {
+    return ships.some((ship) =>
+      ship.getCoordinates().some((coord) => coord[0] === x && coord[1] === y)
+    );
+  };
 };
 
 module.exports = Gameboard;
