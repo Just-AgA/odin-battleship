169c2ddcce2a1e92490b91dd12b70cc458073b5d
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-07-16 20:36:30 +0200

    Test if all ships are sunk

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -29,4 +29,12 @@ describe('Gameboard', () => {
     expect(gameboard.receiveAttack(0, 0)).toBe('hit');
     expect(gameboard.receiveAttack(5, 5)).toBe('miss');
   });
+
+  test('detects all ships sunk', () => {
+    const ship = Ship(2);
+    gameboard.placeShip(ship, 0, 0, true);
+    gameboard.receiveAttack(0, 0);
+    gameboard.receiveAttack(1, 0);
+    expect(gameboard.allShipsSunk()).toBe(true);
+  });
 });
