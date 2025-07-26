41049f998c03f33d6b38bf3b702fe8dd9f6a6560
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-07-26 11:10:48 +0200

    Test if the player can attack the enemy board and hit the ship

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -9,4 +9,13 @@ describe('Player', () => {
     player = Player(false);
     enemy = Player(false);
   });
+
+  test('attacks enemy board', () => {
+    const enemyBoard = enemy.getGameboard();
+    const ship = Ship(1);
+    enemyBoard.placeShip(ship, 0, 0, true);
+
+    const result = player.attack(enemyBoard, 0, 0);
+    expect(result).toBe('hit');
+  });
 });
