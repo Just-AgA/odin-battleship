5806669814ad617d2805da7b0bdea2abd2aad271
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-07-26 11:09:48 +0200

    Add a beforeEach statement when running the tests

---
+++ src/tests/player.test.js
@@ -0,0 +1,12 @@
+const Player = require('../player');
+const Gameboard = require('../gameboard');
+const Ship = require('../ship');
+
+describe('Player', () => {
+  let player, enemy;
+
+  beforeEach(() => {
+    player = Player(false);
+    enemy = Player(false);
+  });
+});
