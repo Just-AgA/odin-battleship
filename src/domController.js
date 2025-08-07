b54bd9f7bb0e7f8be3aeb79f9b536cd3294f1134
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-08-07 15:16:11 +0200

    Render initial gameboard

--- src/domController.js
+++ src/domController.js
@@ -7,4 +7,22 @@ const DomController = (() => {
 
   const playerBoardContainer = document.querySelector('#player-board');
   const computerBoardContainer = document.querySelector('#computer-board');
+
+  const renderBoard = (boardContainer, gameboard, isEnemy = false) => {
+    boardContainer.innerHTML = '';
+    for (let y = 0; y < 10; y++) {
+      for (let x = 0; x < 10; x++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.dataset.x = x;
+        cell.dataset.y = y;
+
+        const isShipHere = gameboard
+          .getShips()
+          .some((ship) =>
+            ship
+              .getCoordinates()
+              .some((coord) => coord[0] === x && coord[1] === y)
+          );
+
 })();
